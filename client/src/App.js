8c13ab256ae521be9371c3ac30f7fdb81c222881
Yin-Lynn-Htun <yinlynnhtun@gmail.com>
2021-07-22 12:34:48 +0630

    #10 feat: Register Page and route setup

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css'
 import Login from './pages/Login/Login'
+import Register from './pages/Register/Register'
 
 function App() {
     return (
@@ -10,7 +11,9 @@ function App() {
                 <Route path="/login">
                     <Login />
                 </Route>
-                <Route path="/register"></Route>
+                <Route path="/register">
+                    <Register />
+                </Route>
                 <Route path="/"></Route>
             </Switch>
         </Router>
---
+++ client/src/pages/Register/Register.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import classes from './Register.module.css'
+import IntroSection from '../../components/IntroSection/IntroSection'
+import RegisterForm from './RegisterForm'
+
+const Register = () => {
+    return (
+        <div className={classes.container}>
+            <IntroSection />
+            <section className={classes.split}>
+                <div className={classes.content}>
+                    <p className={classes.title}>Hello!</p>
+                    <p className={classes.subtitle}>Sign Up to Get Started</p>
+                    <RegisterForm />
+                    <p className={classes.subtitle}>
+                        Already a member?{'  '}
+                        <span className={classes.link}>Login Here</span>
+                    </p>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Register
