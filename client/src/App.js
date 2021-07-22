01f6427e9d08c7f979de53f1add246c2c4d231b5
Yin-Lynn-Htun <yinlynnhtun@gmail.com>
2021-07-22 12:29:39 +0630

    #9 feat: Login Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './App.css'
+import Login from './pages/Login/Login'
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">Hello World</header>
+            <Login />
         </div>
     )
 }
---
+++ client/src/pages/Login/Login.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import IntroSection from '../../components/IntroSection/IntroSection'
+import classes from './Login.module.css'
+import LoginForm from './LoginForm'
+
+const Login = () => {
+    return (
+        <div className={classes.container}>
+            <IntroSection />
+            <section className={classes.split}>
+                <div className={classes.content}>
+                    <p className={classes.title}>Hello Again!</p>
+                    <p className={classes.subtitle}>Welcome Back</p>
+                    <LoginForm />
+                    <p className={classes.subtitle}>
+                        Not a member?{' '}
+                        <span className={classes.link}>Sign Up Here</span>
+                    </p>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Login
