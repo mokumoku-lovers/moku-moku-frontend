2231ce078268bd6d1e3809fd44e2429e046520a4
Yin-Lynn-Htun <yinlynnhtun@gmail.com>
2021-08-06 22:47:55 +0630

    #24 feat: added validation logic for login form

--- src/pages/Login/LoginForm.js
+++ src/pages/Login/LoginForm.js
@@ -3,21 +3,47 @@ import Button from '../../components/UI/FormButton/FormButton'
 import Input from '../../components/UI/Input/Input'
 import classes from './Login.module.css'
 
+const emailRegex = /^[\w-\\.]+@([\w-]+\.)+[\w-]{2,4}$/
+
 const LoginForm = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
+    const [emailIsTouch, setEmailIsTouch] = useState(false)
+    const [passwordIsTouch, setPasswordIsTouch] = useState(false)
+
+    const emailIsValid =
+        email.trim().length > 0 && emailRegex.test(String(email).toLowerCase())
+
+    const emailIsTouchAndInvalid = emailIsTouch && !emailIsValid
+
+    const passwordIsValid = password.trim().length > 0
+    const passwordIsTouchAndInvalid = passwordIsTouch && !passwordIsValid
+
+    const overallFormIsValid = emailIsValid && passwordIsValid
+
     const onEmailChangeHandler = (e) => {
         setEmail(e.target.value)
+        setEmailIsTouch(true)
     }
 
     const onPasswordChangeHandler = (e) => {
         setPassword(e.target.value)
+        setPasswordIsTouch(true)
     }
 
     const onSubmitFormHandler = (e) => {
-        e.preventDefault()
-        alert('Login')
+        if (overallFormIsValid) {
+            e.preventDefault()
+            alert('Login successful')
+
+            setEmail('')
+            setPassword('')
+            setEmailIsTouch(false)
+            setPasswordIsTouch(false)
+        } else {
+            return
+        }
     }
 
     return (
@@ -30,6 +56,7 @@ const LoginForm = () => {
                 placeholder={'Email Address'}
                 fontclassname="fas fa-envelope"
                 onChange={onEmailChangeHandler}
+                isInvalid={emailIsTouchAndInvalid}
                 required
             />
             <Input
@@ -40,9 +67,10 @@ const LoginForm = () => {
                 value={password}
                 fontclassname="fas fa-unlock-alt"
                 onChange={onPasswordChangeHandler}
+                isInvalid={passwordIsTouchAndInvalid}
                 required
             />
-            <Button text="Login" />
+            <Button text="Login" valid={overallFormIsValid} />
             <p className={classes.forgetPassword}>Forget Password</p>
         </form>
     )
