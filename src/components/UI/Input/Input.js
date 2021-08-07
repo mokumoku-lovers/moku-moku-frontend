a5b24eed4ced4b0e2e8bc3f56607bb8b0ff3b76e
Yin-Lynn-Htun <yinlynnhtun@gmail.com>
2021-08-07 19:32:18 +0630

    #24 feat: added showPassword icon and logic

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,11 +1,24 @@
 // create a html input element with reactjs hook with arrow function
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import classes from './Input.module.css'
 
 const Input = (props) => {
+    const [showPassword, setShowPassword] = useState(false)
+    const inputRef = useRef()
+
+    const togglePassword = () => {
+        if (showPassword) {
+            inputRef.current.type = 'password'
+        } else {
+            inputRef.current.type = 'text'
+        }
+        setShowPassword(!showPassword)
+    }
+
     return (
         <div className={classes.container}>
             <input
+                ref={inputRef}
                 {...props}
                 onChange={props.onChange}
                 className={`${classes.formInput} ${
@@ -13,6 +26,13 @@ const Input = (props) => {
                 }`}
             />
             <i className={`${classes.icon} ${props.fontclassname}`}></i>
+            {props.type === 'password' && (
+                <i
+                    onClick={togglePassword}
+                    className={`${
+                        showPassword ? 'fas fa-eye-slash' : 'fas fa-eye'
+                    } ${classes.showPassword}`}></i>
+            )}
         </div>
     )
 }
