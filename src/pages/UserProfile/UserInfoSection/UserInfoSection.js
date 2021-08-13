d64eff3624abb2675e2380f343b1e191249b86c0
Yin-Lynn-Htun <yinlynnhtun@gmail.com>
2021-08-14 01:08:10 +0630

    #26 feat: Basic Layout for UserProfile

---
+++ src/pages/UserProfile/UserInfoSection/UserInfoSection.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from './UserInfoSection.module.css'
+
+const UserInfoSection = () => {
+    return (
+        <div className={classes.container }>
+            <image className={classes.avatar} src="" />
+        </div>
+    )
+}
+
+export default UserInfoSection
