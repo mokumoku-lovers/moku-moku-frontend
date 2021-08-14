cccca54bddc53c22ba8c42c0b3984de3680ec934
Yin-Lynn-Htun <yinlynnhtun@gmail.com>
2021-08-14 11:03:58 +0630

    #26 feat: Added action buttons for user profile

--- src/pages/UserProfile/UserInfoSection/UserInfoSection.js
+++ src/pages/UserProfile/UserInfoSection/UserInfoSection.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Button from '../../../components/UI/Button/Button'
 import classes from './UserInfoSection.module.css'
 
 const UserInfoSection = () => {
@@ -6,11 +7,15 @@ const UserInfoSection = () => {
         <div className={classes.container }>
             <image className={classes.avatar} src="" />
             <div className={classes.user_info__container }>
-                <h1>User#8321</h1>
+                <h1 className={classes.user__info__name}>User#8321</h1>
                 <p>Seize the day</p>
                 <div className={classes.user_point}>
                     <span>POINT <i className={`fas fa-trophy ${classes.trophy_icon}`}></i></span>
                     <span>7000 PT</span>
+                </div>
+                <div className={classes.user_info__actions}>
+                    <Button>Create</Button>
+                    <Button>Edit</Button>
                 </div>
             </div>
         </div>
