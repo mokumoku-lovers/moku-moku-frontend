b8be5f7ce111455f6142807a9a49b63b3e2ce63a
Yin-Lynn-Htun <yinlynnhtun@gmail.com>
2021-08-14 10:28:41 +0630

    #26 feat: added basic info for profile

--- src/pages/UserProfile/UserInfoSection/UserInfoSection.js
+++ src/pages/UserProfile/UserInfoSection/UserInfoSection.js
@@ -5,6 +5,14 @@ const UserInfoSection = () => {
     return (
         <div className={classes.container }>
             <image className={classes.avatar} src="" />
+            <div className={classes.user_info__container }>
+                <h1>User#8321</h1>
+                <p>Seize the day</p>
+                <div className={classes.user_point}>
+                    <span>POINT</span>
+                    <span>7000 PT</span>
+                </div>
+            </div>
         </div>
     )
 }
