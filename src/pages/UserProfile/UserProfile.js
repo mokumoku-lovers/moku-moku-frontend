97a1f94860b945063c5cc1c38003ff0d427d1134
Yin-Lynn-Htun <yinlynnhtun@gmail.com>
2021-08-14 12:10:29 +0630

    #27 feat: Added UserCardSection in userprofile

--- src/pages/UserProfile/UserProfile.js
+++ src/pages/UserProfile/UserProfile.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import UserCardSection from './UserCardSection/UserCardSection'
 import UserInfoSection from './UserInfoSection/UserInfoSection'
 import classes from './UserProfile.module.css'
 
@@ -7,6 +8,7 @@ const UserProfile = () => {
         <div className={classes.container}>
             <div className={classes.user_profile__section}>
                 <UserInfoSection />
+                <UserCardSection />
             </div>
         </div>
     )
