c6e7bd3336d6ab3269033e71936d8c1b5cc85032
Yin Lynn Htun <yinlynnhtun@gmail.com>
2021-09-16 21:59:22 +0630

    #19 refactor: Change the searchbar as react componet

--- src/components/NavBar/SearchBar.js
+++ src/components/NavBar/SearchBar.js
@@ -1,34 +1,16 @@
-import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
+import React from "react";
 import "./SearchBar.css";
-import logo from "../../logo.svg"
 import icon from "../../icon.svg"
 
-function SearchBar() {
-  const [click, setClick] = useState(false);
-
-  const handleClick = () => setClick(!click);
+function SearchBar({click}) {
   return (
-    <>
-      <nav className="navbar">
-        <div className="nav-container">
-          <NavLink exact to="/" className="nav-logo">
-            <img src={logo} alt="MokuMoku" />
-          </NavLink>
-
-          <ul className={click ? "nav-menu active" : "nav-menu"}>
-            <div className="input-control">
-              <input type="text" className="searchbar" />
-              <i className="fas fa-search search-icon"></i>
-            </div>
-            <img src={icon} alt="Icon" />
-          </ul>
-          <div className="nav-icon" onClick={handleClick}>
-            <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
-          </div>
+      <ul className={click ? "nav-menu active" : "nav-menu"}>
+        <div className="input-control">
+          <input type="text" className="searchbar" />
+          <i className="fas fa-search search-icon"></i>
         </div>
-      </nav>
-    </>
+        <img src={icon} alt="Icon" />
+      </ul>
   );
 }
 
