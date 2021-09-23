c28a29a7af1d3dcdbc9bebda688520a0214799e8
Yin Lynn Htun <yinlynnhtun@gmail.com>
2021-09-23 21:45:05 +0630

    #34 feat: Intial setup for ranking page

---
+++ src/pages/RankingPage/RankingPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import classes from './RankingPage.module.css'
+
+const RankingPage = () => {
+    return (
+        <div>
+            <h1>Leaderboard</h1>
+            <section>
+
+            </section>
+        </div>
+    )
+}
+
+export default RankingPage
