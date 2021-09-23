0e9e572b010b420e66104649ea4efa0e029b7f20
Yin Lynn Htun <yinlynnhtun@gmail.com>
2021-09-23 22:34:53 +0630

    #35 feat: Add alignment and font styles

--- src/pages/RankingPage/RankingPage.js
+++ src/pages/RankingPage/RankingPage.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import RankingListItem from './RankingListItem'
 import classes from './RankingPage.module.css'
 
 const RankingPage = () => {
     return (
         <div className={classes.container}>
             <h1>Leaderboard</h1>
-            <section></section>
+            <section>
+                <RankingListItem />
+                <RankingListItem />
+                <RankingListItem />
+                <RankingListItem />
+                <RankingListItem />
+                <RankingListItem />
+                <RankingListItem />
+                <RankingListItem />
+                <RankingListItem />
+            </section>
         </div>
     )
 }
