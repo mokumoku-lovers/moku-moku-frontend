90d581953dc7d3e297e73d1c36f8cd99e43d08cf
Yin Lynn Htun <yinlynnhtun@gmail.com>
2021-09-23 21:45:25 +0630

    #34 feat: Add route for ranking page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css'
 import Homepage from './pages/Homepage/Homepage'
 import Login from './pages/Login/Login'
+import RankingPage from './pages/RankingPage/RankingPage'
 import Register from './pages/Register/Register'
 
 function App() {
@@ -14,6 +15,9 @@ function App() {
                 </Route>
                 <Route path="/register">
                     <Register />
+                </Route>
+                <Route path="/ranking">
+                    <RankingPage />
                 </Route>
                 <Route path="/">
                     <Homepage />
