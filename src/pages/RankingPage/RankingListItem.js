68027477533feecd64e5cbca2a03a9042c0d1791
Yin Lynn Htun <yinlynnhtun@gmail.com>
2021-09-23 22:43:26 +0630

    #35 feat: Add react props

--- src/pages/RankingPage/RankingListItem.js
+++ src/pages/RankingPage/RankingListItem.js
@@ -1,17 +1,13 @@
 import React from 'react'
 import classes from './RankingListItem.module.css'
 
-const RankingListItem = () => {
+const RankingListItem = ({ rank, img_src, name, point }) => {
     return (
         <div className={classes.container}>
-            <p className={classes.rank_number}>1st</p>
-            <img
-                className={classes.avatar}
-                src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxhbGx8fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit"
-                alt="user profile"
-            />
-            <p className={classes.name}>Hatori</p>
-            <p className={classes.point}>7000 PT</p>
+            <p className={classes.rank_number}>{rank}</p>
+            <img className={classes.avatar} src={img_src} alt="user profile" />
+            <p className={classes.name}>{name}</p>
+            <p className={classes.point}>{point} PT</p>
         </div>
     )
 }
