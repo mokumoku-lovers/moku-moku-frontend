88db4e36877ec51ba9641070e619e4f3c5f82755
Yin Lynn Htun <yinlynnhtun@gmail.com>
2021-09-25 00:36:09 +0630

    #36 feat: Add styling for button secondary component

--- src/components/UI/Button/ButtonSecondary.js
+++ src/components/UI/Button/ButtonSecondary.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import classes from './ButtonSecondary.module.css'
 
-const ButtonSecondary = () => {
-    return <div></div>
+const ButtonSecondary = (props) => {
+    return (
+        <button {...props} className={classes.button}>
+            {props.children}
+        </button>
+    )
 }
 
 export default ButtonSecondary
