10fbb52e2a54cd4a0971f4e6b8df7661ce5f4b3d
Yin Lynn Htun <yinlynnhtun@gmail.com>
2021-09-30 14:37:22 +0630

    #34: Edit avatar component to have gradient border

--- src/components/UI/Avatar/Avatar.js
+++ src/components/UI/Avatar/Avatar.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import classes from './Avatar.module.css'
 
-export const Avatar = () => {
-    return <img className={classes.avatar} alt="User Profile" src="" />
+export const Avatar = (prop) => {
+    return (
+        <img
+            className={`${prop.className} ${classes.avatar}`}
+            alt="User Profile"
+            src=""
+        />
+    )
 }
