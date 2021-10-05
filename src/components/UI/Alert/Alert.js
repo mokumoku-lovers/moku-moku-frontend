f893f45790966722cc69a8ca60e9b84ad71bef7c
Yin Lynn Htun <yinlynnhtun@gmail.com>
2021-10-06 00:06:36 +0630

    #30 feat: Create Alert compoent

---
+++ src/components/UI/Alert/Alert.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import classes from './Alert.module.css'
+
+const Alert = (props) => {
+    return (
+        <div className={`${props.className} ${classes.alert}`}>
+            <div className={classes.alert__icon}>
+                <i className={`fas fa-${props.icon}`}></i>
+            </div>
+            <div className={classes.alert__text}>{props.text}</div>
+        </div>
+    )
+}
+
+export default Alert
