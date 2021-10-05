b35dd5606b540234d871fb9b94b96648ba6a3ae0
Yin Lynn Htun <yinlynnhtun@gmail.com>
2021-10-06 00:14:20 +0630

    #30 feat: Added styling for alert

--- src/components/UI/Alert/Alert.js
+++ src/components/UI/Alert/Alert.js
@@ -4,10 +4,10 @@ import classes from './Alert.module.css'
 const Alert = (props) => {
     return (
         <div className={`${props.className} ${classes.alert}`}>
+            <div className={classes.alert__text}>{props.children}</div>
             <div className={classes.alert__icon}>
                 <i className={`fas fa-${props.icon}`}></i>
             </div>
-            <div className={classes.alert__text}>{props.text}</div>
         </div>
     )
 }
