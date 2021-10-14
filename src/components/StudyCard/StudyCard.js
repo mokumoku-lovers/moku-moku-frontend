69642bcc52f522d007216c2c05ebda805735e4f9
JiahuiChen99 <Jiahui1@hotmail.es>
2021-10-14 07:12:50 +0200

    #50 feat: study card front template & styles - Front card UI template - Front card UI styles

---
+++ src/components/StudyCard/StudyCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from './StudyCard.module.css';
+
+const StudyCard = () => {
+    return (
+        <div className={classes.card}>
+            <h1 className={classes.wordFront}> Apple </h1>
+        </div>
+    )
+}
+
+export default StudyCard;
