fb3633680ca8568d053e1f11ba7fdb168b8eb702
JiahuiChen99 <Jiahui1@hotmail.es>
2021-10-14 07:11:14 +0200

    #41 feat: study page layout - Styles for study card section in study page

--- src/pages/Study/Study.js
+++ src/pages/Study/Study.js
@@ -1,10 +1,15 @@
 import React from "react";
+import StudyCard from "../../components/StudyCard/StudyCard";
+import classes from "./Study.module.css";
 
 const Study = () => {
     return (
-        <div>
-
-        </div>
+        <section>
+            <div className={classes.container}>
+                <StudyCard/>
+            </div>
+            {/* Buttons */}
+        </section>
     )
 }
 
