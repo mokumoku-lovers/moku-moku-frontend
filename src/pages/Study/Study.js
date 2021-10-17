ab15bddece195d300ad21348aebf00d1463c4a66
Nicole Wert <nicalexxandra@gmail.com>
2021-10-17 23:40:32 +0900

    #43 feat: add CardButtons component to study route

--- src/pages/Study/Study.js
+++ src/pages/Study/Study.js
@@ -1,9 +1,10 @@
 import React from "react";
+import CardButtons from "../../components/UI/CardButtons/CardButtons";
 
 const Study = () => {
     return (
         <div>
-
+             <CardButtons side="front"/>
         </div>
     )
 }
