9062ac0dd1854bbcccfed74588d486f8d9d172f6
Yin Lynn Htun <yinlynnhtun@gmail.com>
2021-10-21 18:16:27 +0630

    #21 feat: Add General Info Form in editing page

--- src/pages/ProfileEditingPage/ProfileEditingPage.js
+++ src/pages/ProfileEditingPage/ProfileEditingPage.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import GeneralInfoForm from './GeneralInfoForm/GeneralInfoForm'
+import NavBar from '../../components/NavBar/NavBar'
 
 const ProfileEditingPage = () => {
-    return <div>Profile Editing</div>
+    return (
+        <div>
+            <NavBar />
+            Profile Editing
+            <GeneralInfoForm />
+        </div>
+    )
 }
 
 export default ProfileEditingPage
