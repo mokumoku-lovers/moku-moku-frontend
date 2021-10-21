07473007e460836bf238e6ebb9c54000305fbc82
Yin Lynn Htun <yinlynnhtun@gmail.com>
2021-10-21 18:22:50 +0630

    #20 fix: Fix react warning

--- src/pages/ProfileEditingPage/Sidebar/SidebarItem.js
+++ src/pages/ProfileEditingPage/Sidebar/SidebarItem.js
@@ -3,7 +3,7 @@ import classes from './SidebarItem.module.css'
 
 const SidebarItem = (props) => {
     return (
-        <p {...props} className={classes.item}>
+        <p className={classes.item}>
             {props.active && <span className={classes.active}></span>}
             {props.text}
         </p>
