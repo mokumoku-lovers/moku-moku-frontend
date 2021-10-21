332990a726398582be3d182339c4c94b59b6035d
Yin Lynn Htun <yinlynnhtun@gmail.com>
2021-10-21 16:13:37 +0630

    #20 feat: Add Active class in sidebar

--- src/pages/ProfileEditingPage/Sidebar/SidebarItem.js
+++ src/pages/ProfileEditingPage/Sidebar/SidebarItem.js
@@ -4,6 +4,7 @@ import classes from './SidebarItem.module.css'
 const SidebarItem = (props) => {
     return (
         <p {...props} className={classes.item}>
+            {props.active && <span className={classes.active}></span>}
             {props.text}
         </p>
     )
