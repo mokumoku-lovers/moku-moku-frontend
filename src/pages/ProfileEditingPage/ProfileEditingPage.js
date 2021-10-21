edac00a006edbe01f58ce3dfe90b363c71956433
Yin Lynn Htun <yinlynnhtun@gmail.com>
2021-10-21 16:15:44 +0630

    #20 feat: Add Navbar and Sidebar in editing page

--- src/pages/ProfileEditingPage/ProfileEditingPage.js
+++ src/pages/ProfileEditingPage/ProfileEditingPage.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import NavBar from '../../components/NavBar/NavBar'
+import Sidebar from './Sidebar/Sidebar'
 
 const ProfileEditingPage = () => {
-    return <div>Profile Editing</div>
+    return (
+        <div>
+            <NavBar />
+            <Sidebar />
+        </div>
+    )
 }
 
 export default ProfileEditingPage
