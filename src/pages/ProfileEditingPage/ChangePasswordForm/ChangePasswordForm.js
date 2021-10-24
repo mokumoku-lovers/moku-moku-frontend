12ca06e937f587fafbe090d62d4d0991fa8f7b93
Yin Lynn Htun <yinlynnhtun@gmail.com>
2021-10-24 11:53:15 +0630

    #22 feat: Add change password form

--- src/pages/ProfileEditingPage/ChangePasswordForm/ChangePasswordForm.js
+++ src/pages/ProfileEditingPage/ChangePasswordForm/ChangePasswordForm.js
@@ -17,7 +17,48 @@ const ChangePasswordForm = () => {
         setConfirmPassword(event.target.value)
     }
 
-    return <div></div>
+    return (
+        <div className={classes.container}>
+            <form>
+                <div className={classes.row}>
+                    <label htmlFor="old password">Old Password</label>
+                    <Input
+                        className={classes.input}
+                        id="old password"
+                        name="old password"
+                        type="password"
+                        value={oldPassword}
+                        fontclassname="fas fa-unlock-alt"
+                        onChange={onOldPasswordChangeHandler}
+                    />
+                </div>
+                <div className={classes.row}>
+                    <label htmlFor="new password">New Password</label>
+                    <Input
+                        className={classes.input}
+                        id="new password"
+                        name="new password"
+                        type="password"
+                        value={newPassword}
+                        fontclassname="fas fa-unlock-alt"
+                        onChange={onNewPasswordChangeHandler}
+                    />
+                </div>
+                <div className={classes.row}>
+                    <label htmlFor="confirm password">Confirm Password</label>
+                    <Input
+                        className={classes.input}
+                        id="confirm password"
+                        name="confirm password"
+                        type="password"
+                        value={confirmPassword}
+                        fontclassname="fas fa-unlock-alt"
+                        onChange={onConfirmPasswordChangeHandler}
+                    />
+                </div>
+            </form>
+        </div>
+    )
 }
 
 export default ChangePasswordForm
