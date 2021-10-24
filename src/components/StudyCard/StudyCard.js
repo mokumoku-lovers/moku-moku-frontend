4c0be96ffd0636ef38b933bf68481677a4d4706c
JiahuiChen99 <Jiahui1@hotmail.es>
2021-10-24 11:25:45 +0200

    #46 feat: card flip state

--- src/components/StudyCard/StudyCard.js
+++ src/components/StudyCard/StudyCard.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
 import classes from './StudyCard.module.css';
 
 const StudyCard = () => {
+    /*  Card front side: false
+    *   Card back side: true
+    */
+    const [cardState, flipCard] = useState(true)
+
     return (
         <div className={classes.card}>
             <h1 className={classes.wordFront}> Apple </h1>
