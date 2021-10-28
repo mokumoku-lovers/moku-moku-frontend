b0964391d364d6c01c6dd6d44d4694990fcc90d6
Yin Lynn Htun <yinlynnhtun@gmail.com>
2021-10-28 13:58:43 +0630

    feat: Add isEditProfile state in profile editing page

--- src/pages/ProfileEditingPage/ProfileEditingPage.js
+++ src/pages/ProfileEditingPage/ProfileEditingPage.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import GeneralInfoForm from './GeneralInfoForm/GeneralInfoForm'
 import NavBar from '../../components/NavBar/NavBar'
 import Sidebar from './Sidebar/Sidebar'
 import ChangePasswordForm from './ChangePasswordForm/ChangePasswordForm'
 
-const ProfileEditingPage = () => {
+const ProfileEditingPage = (props) => {
+    const [isEditProfile, setIsEditProfile] = useState(
+        props.isEditProfile || true
+    )
+
+    const onClickEditProfile = () => {
+        setIsEditProfile(true)
+    }
+
+    const onClickChangePassword = () => {
+        setIsEditProfile(false)
+    }
+
     return (
         <div>
             <NavBar />
