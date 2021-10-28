7230692dd2da87ea6253b56e3c020113f4dca190
Yin Lynn Htun <yinlynnhtun@gmail.com>
2021-10-28 14:03:28 +0630

    feat: Add layout for profile editing page

--- src/pages/ProfileEditingPage/ProfileEditingPage.js
+++ src/pages/ProfileEditingPage/ProfileEditingPage.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react'
+import classes from './ProfileEditingPage.module.css'
 import GeneralInfoForm from './GeneralInfoForm/GeneralInfoForm'
 import NavBar from '../../components/NavBar/NavBar'
 import Sidebar from './Sidebar/Sidebar'
 import ChangePasswordForm from './ChangePasswordForm/ChangePasswordForm'
+import { Avatar } from '../../components/UI/Avatar/Avatar'
+import { Link } from 'react-router-dom'
 
 const ProfileEditingPage = (props) => {
     const [isEditProfile, setIsEditProfile] = useState(
@@ -18,17 +21,24 @@ const ProfileEditingPage = (props) => {
     }
 
     return (
-        <div>
+        <>
             <NavBar />
-
-            <Sidebar
-                isEditProfile={isEditProfile}
-                onClickEditProfile={onClickEditProfile}
-                onClickChangePassword={onClickChangePassword}
-            />
-
-            {isEditProfile ? <GeneralInfoForm /> : <ChangePasswordForm />}
-        </div>
+            <section className={classes.container}>
+                <Sidebar
+                    isEditProfile={isEditProfile}
+                    onClickEditProfile={onClickEditProfile}
+                    onClickChangePassword={onClickChangePassword}
+                />
+                <div className={classes.separator} />
+                <div className={classes.formSection}>
+                    {isEditProfile ? (
+                        <GeneralInfoForm />
+                    ) : (
+                        <ChangePasswordForm />
+                    )}
+                </div>
+            </section>
+        </>
     )
 }
 
