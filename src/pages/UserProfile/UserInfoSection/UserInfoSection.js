b4c93571ddf87b29d75e0b377cdebfd980176839
Yin Lynn Htun <yinlynnhtun@gmail.com>
2021-11-03 15:25:30 +0630

    fix: Use Avatar component

--- src/pages/UserProfile/UserInfoSection/UserInfoSection.js
+++ src/pages/UserProfile/UserInfoSection/UserInfoSection.js
@@ -2,11 +2,12 @@ import React from 'react'
 import Button from '../../../components/UI/Button/Button'
 import classes from './UserInfoSection.module.css'
 import Point from '../../../components/UI/Point/Point'
+import { Avatar } from '../../../components/UI/Avatar/Avatar'
 
 const UserInfoSection = () => {
     return (
         <div className={classes.container}>
-            <img className={classes.avatar} alt="User Profile" src="" />
+            <Avatar className={classes.avatar} src="" />
             <div className={classes.user_info__container}>
                 <h1 className={classes.user__info__name}>User#8321</h1>
                 <p>Seize the day</p>
