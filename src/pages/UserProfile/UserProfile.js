f275208db95de6346a2bea01b18d0554c013a7bb
Yin Lynn Htun <yinlynnhtun@gmail.com>
2021-11-03 15:30:13 +0630

    #42 feat: Add navBar in profile page

--- src/pages/UserProfile/UserProfile.js
+++ src/pages/UserProfile/UserProfile.js
@@ -2,15 +2,19 @@ import React from 'react'
 import UserCardSection from './UserCardSection/UserCardSection'
 import UserInfoSection from './UserInfoSection/UserInfoSection'
 import classes from './UserProfile.module.css'
+import NavBar from '../../components/NavBar/NavBar'
 
 const UserProfile = () => {
     return (
-        <div className={classes.container}>
-            <div className={classes.user_profile__section}>
-                <UserInfoSection />
-                <UserCardSection />
+        <>
+            <NavBar />
+            <div className={classes.container}>
+                <div className={classes.user_profile__section}>
+                    <UserInfoSection />
+                    <UserCardSection />
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
