3b45c02b566bca50d4b52fa8bafbb716fa8ce229
Yin Lynn Htun <yinlynnhtun@gmail.com>
2021-11-03 15:22:26 +0630

    #42 feat: Use Point component

--- src/pages/UserProfile/UserInfoSection/UserInfoSection.js
+++ src/pages/UserProfile/UserInfoSection/UserInfoSection.js
@@ -1,18 +1,16 @@
 import React from 'react'
 import Button from '../../../components/UI/Button/Button'
 import classes from './UserInfoSection.module.css'
+import Point from '../../../components/UI/Point/Point'
 
 const UserInfoSection = () => {
     return (
         <div className={classes.container}>
             <img className={classes.avatar} alt="User Profile" src="" />
-            <div className={classes.user_info__container }>
+            <div className={classes.user_info__container}>
                 <h1 className={classes.user__info__name}>User#8321</h1>
                 <p>Seize the day</p>
-                <div className={classes.user_point}>
-                    <span>POINT <i className={`fas fa-trophy ${classes.trophy_icon}`}></i></span>
-                    <span>7000 PT</span>
-                </div>
+                <Point point={7000} />
                 <div className={classes.user_info__actions}>
                     <Button>Create</Button>
                     <Button>Edit</Button>
