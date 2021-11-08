35e667e43adbf52e67e36aeeb4b74dbdbcbbbef5
Yin Lynn Htun <yinlynnhtun@gmail.com>
2021-11-08 10:31:35 +0630

    #42 feat: Add profile picture in profile page

--- src/pages/UserProfile/UserInfoSection/UserInfoSection.js
+++ src/pages/UserProfile/UserInfoSection/UserInfoSection.js
@@ -3,11 +3,12 @@ import Button from '../../../components/UI/Button/Button'
 import classes from './UserInfoSection.module.css'
 import Point from '../../../components/UI/Point/Point'
 import { Avatar } from '../../../components/UI/Avatar/Avatar'
+import icon from '../../../icon.svg'
 
 const UserInfoSection = () => {
     return (
         <div className={classes.container}>
-            <Avatar className={classes.avatar} src="" />
+            <Avatar className={classes.avatar} src={icon} />
             <div className={classes.user_info__container}>
                 <h1 className={classes.user__info__name}>User#8321</h1>
                 <p className={classes.user__info__bio}>Seize the day</p>
