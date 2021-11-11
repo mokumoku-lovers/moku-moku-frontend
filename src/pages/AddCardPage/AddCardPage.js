002a8dc991f99272a99ed49dc5dd1d8d09eb2265
Yin Lynn Htun <yinlynnhtun@gmail.com>
2021-11-11 19:14:32 +0630

    #69 feat: Create component for add card page

---
+++ src/pages/AddCardPage/AddCardPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from '../../components/NavBar/NavBar'
+
+const AddCardPage = () => {
+    return (
+        <div>
+            <NavBar />
+            Add card page
+        </div>
+    )
+}
+
+export default AddCardPage
