a6c64622f5500a5e33dfac35d161e5e780fbd09e
Yin Lynn Htun <yinlynnhtun@gmail.com>
2021-11-25 16:52:48 +0630

    feat: Add react drop zone hook

--- src/pages/AddCardPage/AddCardPageImageField/ImageField.js
+++ src/pages/AddCardPage/AddCardPageImageField/ImageField.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import classes from './ImageField.module.css'
 import Button from '../../../components/UI/Button/Button'
+import { useDropzone } from 'react-dropzone'
 
 const ImageField = () => {
+    const { getRootProps, getInputProps, isDragActive, open } = useDropzone({
+        noClick: true,
+        noKeyboard: true,
+        accept: 'image/jpeg, image/png',
+    })
+
     return (
         <section className={classes.container}>
             <input type="file" />
             <p className={classes.formText}>
                 Drag & Drop to Upload Image <span>OR</span>
-                <Button className={classes.btn}>Browse Images</Button>
+                <Button type="button" onClick={open} className={classes.btn}>
+                    Browse Images
+                </Button>
             </p>
         </section>
     )
