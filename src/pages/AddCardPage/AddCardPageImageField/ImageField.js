e5afe49150dc0094564bda306468cc8ecd01928f
Yin Lynn Htun <yinlynnhtun@gmail.com>
2021-11-25 17:01:12 +0630

    feat: Added files state and ondrop function

--- src/pages/AddCardPage/AddCardPageImageField/ImageField.js
+++ src/pages/AddCardPage/AddCardPageImageField/ImageField.js
@@ -1,24 +1,44 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import classes from './ImageField.module.css'
 import Button from '../../../components/UI/Button/Button'
 import { useDropzone } from 'react-dropzone'
 
 const ImageField = () => {
+    const [files, setFiles] = useState([])
+
     const { getRootProps, getInputProps, isDragActive, open } = useDropzone({
         noClick: true,
         noKeyboard: true,
         accept: 'image/jpeg, image/png',
     })
 
+    const onDrop = useCallback((acceptedFiles) => {
+        setFiles(
+            acceptedFiles.map((file, idx) => (
+                <p className={classes.formText} key={idx}>
+                    {file.name}
+                </p>
+            ))
+        )
+    }, [])
+
     return (
-        <section className={classes.container}>
-            <input type="file" />
-            <p className={classes.formText}>
-                Drag & Drop to Upload Image <span>OR</span>
-                <Button type="button" onClick={open} className={classes.btn}>
-                    Browse Images
-                </Button>
-            </p>
+        <section className={classes.container} {...getRootProps()}>
+            <input {...getInputProps()} />
+            {isDragActive ? (
+                <p className={classes.formText}>Drop the image here ...</p>
+            ) : (
+                <p className={classes.formText}>
+                    Drag & Drop to Upload Image <span>OR</span>
+                    <Button
+                        type="button"
+                        onClick={open}
+                        className={classes.btn}
+                    >
+                        Browse Images
+                    </Button>
+                </p>
+            )}
         </section>
     )
 }
