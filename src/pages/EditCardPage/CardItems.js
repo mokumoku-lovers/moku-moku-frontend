2eb83d1de52a6e4a19e12cad9d4176cb00799abd
Yin Lynn Htun <yinlynnhtun@gmail.com>
2021-12-13 01:04:41 +0630

    #76 feat: Add basic container for cards

---
+++ src/pages/EditCardPage/CardItems.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CardItems = () => {
+    return (
+        <div className={classes.container}>
+            <h1>Card Front</h1>
+            <h1>Card Back</h1>
+        </div>
+    )
+}
+
+export default CardItems
