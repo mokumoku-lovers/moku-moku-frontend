55ee7f4a23a19196d89ecf2eb3360f588ea69149
Yin Lynn Htun <yinlynnhtun@gmail.com>
2021-12-12 23:31:44 +0630

    #75 feat: Add styling for container

---
+++ src/pages/EditCardPage/EditCardPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from '../../components/NavBar/NavBar'
+import classes from './EditCardPage.module.css'
+
+const EditCardPage = () => {
+    return (
+        <div className={classes.container}>
+            <NavBar/>
+        </div>
+    )
+}
+
+export default EditCardPage
