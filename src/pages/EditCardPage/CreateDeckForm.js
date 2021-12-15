bbe19cb4d340c17a197ed5b4340c96196226f77a
Yin Lynn Htun <yinlynnhtun@gmail.com>
2021-12-15 16:03:13 +0630

    #77 feat: Add title state in form and bind state value to form input

--- src/pages/EditCardPage/CreateDeckForm.js
+++ src/pages/EditCardPage/CreateDeckForm.js
@@ -4,18 +4,32 @@ import Input from '../../components/UI/Input/Input'
 import Button from '../../components/UI/Button/Button'
 import ButtonSecondary from '../../components/UI/Button/ButtonSecondary'
 import Model from '../../components/UI/Model/Model'
+import { useState } from 'react'
 
 const CreateDeckForm = (props) => {
+    const [title, setTitle] = useState('')
+
+    const onChangeTitle = (e) => {
+        setTitle(e.target.value)
+    }
+
     return (
         <Model>
-            <form className={classes.form}>
+            <form
+                className={classes.form}
+                onSubmit={(e) => props.onClickCreateDeck(e, title)}
+            >
                 <h1>Create Your Deck</h1>
 
                 <div className={classes.formControl}>
                     <label htmlFor="title">
                         Deck Title <span style={{ color: 'red' }}>*</span>
                     </label>
-                    <Input placeholder="Enter your deck title" />
+                    <Input
+                        value={title}
+                        placeholder="Enter your deck title"
+                        onChange={onChangeTitle}
+                    />
                 </div>
 
                 <div className={classes.buttons}>
