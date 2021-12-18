69752ff436040d708637f8c69c70d83007782fb4
Yin Lynn Htun <yinlynnhtun@gmail.com>
2021-12-19 01:01:23 +0630

    #77 feat: Add buttons

--- src/pages/EditCardPage/EditCardPageHeader.js
+++ src/pages/EditCardPage/EditCardPageHeader.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import Button from '../../components/UI/Button/Button'
 import classes from './EditCardPageHeader.module.css'
 
 const EditCardPageHeader = (props) => {
@@ -9,6 +9,10 @@ const EditCardPageHeader = (props) => {
                     <p className={classes.deck__title}>Japanese</p>
                 </div>
                 <p className={classes.card__count}>0 card</p>
+            </div>
+            <div className={classes.card__buttons}>
+                <Button onClick={clickAddCardButton}>Add Card</Button>
+                <Button onClick={clickStudyButton}>Study</Button>
             </div>
         </div>
     )
