2f7cdf4135288d5df2d57f26fa4e5362f40d8d9e
Yin Lynn Htun <yinlynnhtun@gmail.com>
2021-12-19 01:03:07 +0630

    #77 feat: Add editing state, and icons

--- src/pages/EditCardPage/EditCardPageHeader.js
+++ src/pages/EditCardPage/EditCardPageHeader.js
@@ -2,11 +2,22 @@ import Button from '../../components/UI/Button/Button'
 import classes from './EditCardPageHeader.module.css'
 
 const EditCardPageHeader = (props) => {
+    const [editing, setEditing] = useState(false)
     return (
         <div className={classes.container}>
             <div>
                 <div>
-                    <p className={classes.deck__title}>Japanese</p>
+                    {editing ? (
+                        <i
+                            className={`far fa-save ${classes.icon} ${classes.iconSave}`}
+                            onClick={onClickTitleSaveHandler}
+                        ></i>
+                    ) : (
+                        <i
+                            onClick={onClickTitleEditHandler}
+                            className={`fas fa-edit ${classes.icon} ${classes.iconEdit}`}
+                        ></i>
+                    )}
                 </div>
                 <p className={classes.card__count}>0 card</p>
             </div>
