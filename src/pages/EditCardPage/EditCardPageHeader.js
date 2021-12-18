7070b211c7f758470350f0a4aee1fd8ea3132b19
Yin Lynn Htun <yinlynnhtun@gmail.com>
2021-12-19 01:04:32 +0630

    #feat: Add edit and save onClick handler

--- src/pages/EditCardPage/EditCardPageHeader.js
+++ src/pages/EditCardPage/EditCardPageHeader.js
@@ -1,8 +1,27 @@
+import React, { useEffect, useState, useRef } from 'react'
 import Button from '../../components/UI/Button/Button'
 import classes from './EditCardPageHeader.module.css'
 
 const EditCardPageHeader = (props) => {
+    const deckRef = useRef()
     const [editing, setEditing] = useState(false)
+    const onClickTitleEditHandler = () => {
+        setEditing(true)
+
+        // focus to edit deck's title when user click edit icon
+        // normal deckRef.current.focus() is not working so I used setTimeout
+        setTimeout(function () {
+            deckRef.current.focus()
+        }, 0)
+
+        deckRef.current.style.color = '#a9a9a9'
+    }
+
+    const onClickTitleSaveHandler = () => {
+        setEditing(false)
+        setDeckTitle(deckRef.current.innerText)
+        deckRef.current.style.color = 'black'
+    }
     return (
         <div className={classes.container}>
             <div>
