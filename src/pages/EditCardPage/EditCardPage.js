875925e4018d5b1439795c23be6cb4b65927f29c
Yin Lynn Htun <yinlynnhtun@gmail.com>
2021-12-20 20:32:28 +0630

    #77 refactor: Remove CreateDeckForm from editCardPage

--- src/pages/EditCardPage/EditCardPage.js
+++ src/pages/EditCardPage/EditCardPage.js
@@ -1,27 +1,13 @@
-import React, { useState } from 'react'
+import React from 'react'
 import NavBar from '../../components/NavBar/NavBar'
 import classes from './EditCardPage.module.css'
-import CreateDeckForm from './CreateDeckForm'
 import EditCardPageHeader from './EditCardPageHeader'
 
 const EditCardPage = (props) => {
-    const [deckTitle, setTitle] = useState('')
-    const [showDeckForm, setShowDeckForm] = useState(true)
-
-    const onClickCreateDeck = (e, title) => {
-        e.preventDefault()
-        console.log(title)
-        setTitle(title)
-        setShowDeckForm(false)
-    }
-
     return (
         <div className={classes.container}>
             <NavBar />
-            {showDeckForm && (
-                <CreateDeckForm onClickCreateDeck={onClickCreateDeck} />
-            )}
-            <EditCardPageHeader title={deckTitle} />
+            <EditCardPageHeader />
         </div>
     )
 }
