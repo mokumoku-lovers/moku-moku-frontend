2ffc5a589b17c7688837a6d80d6932df1686c0f4
Yin Lynn Htun <yinlynnhtun@gmail.com>
2021-12-28 13:01:51 +0630

    #82 feat: Add react router link for avatar and settins

--- src/components/ProfileCard/ProfileCard.js
+++ src/components/ProfileCard/ProfileCard.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import Icon from '../../icon.svg'
 import classes from './ProfileCard.module.css'
+import { Link } from 'react-router-dom'
 
 const ProfileCard = () => {
     return (
         <div className={classes.container}>
             <div className={classes.info__container}>
-                <img src={Icon} alt="Avatar" />
-
+                <Link to="/profile/" className={classes.avator}>
+                    <img src={Icon} alt="Avatar" />
+                </Link>
                 <h3 className={classes.username}>Username</h3>
                 <p className={classes.email}>smileycat@gmail.com</p>
             </div>
-            <p className={classes.settings}>Settings</p>
+            <Link to="/edit-profile/" className={classes.settings}>
+                Settings
+            </Link>
             <div className={classes.logout}>Logout</div>
         </div>
     )
