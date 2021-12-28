5b58091d4e75dd332f6b08689fcab61ae0e11ce0
Yin Lynn Htun <yinlynnhtun@gmail.com>
2021-12-28 13:03:55 +0630

    #82 feat: Add showProfile state and toggle function

--- src/components/NavBar/AuthNav.js
+++ src/components/NavBar/AuthNav.js
@@ -1,15 +1,26 @@
 import React from 'react'
 import classes from './NavBar.module.css'
 import icon from '../../icon.svg'
+import { useState } from 'react'
+import ProfileCard from '../ProfileCard/ProfileCard'
 
 const AuthNav = () => {
+    const [showProfile, setShowProfile] = useState(false)
+
     return (
         <ul className={classes['nav-menu']}>
             <div className={classes['input-control']}>
                 <input type="text" className={classes.searchbar} />
                 <i className={`fas fa-search ${classes['search-icon']}`}></i>
             </div>
-            <img src={icon} alt="Icon" className={classes['user-avatar']} />
+            <div className={classes.avatar_container}>
+                <img
+                    src={icon}
+                    alt="Icon"
+                    className={classes['user-avatar']}
+                    onClick={() => setShowProfile(!showProfile)}
+                />
+            </div>
         </ul>
     )
 }
