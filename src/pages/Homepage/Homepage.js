d80265ccdc9d64935702a6dbe934925f22f9612d
Yin Lynn Htun <yinlynnhtun@gmail.com>
2022-01-04 14:41:07 +0630

    #87 feat: Add html contents for homepage

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import NavBar from '../../components/NavBar/NavBar'
+import classes from './Homepage.module.css'
+import illustration from './studying.svg'
 
 const Homepage = () => {
     return (
-        <div>
-            <NavBar />
-        </div>
+        <>
+            <NavBar isLoggedIn={false} />
+            <section>
+                <div>
+                    <h1>Hello! We're</h1>
+                    <h1>Moku Moku</h1>
+                    <h1>Learn it. Remember it.</h1>
+                    <button>Get Started</button>
+                </div>
+                <div>
+                    <img src={illustration} alt="" />
+                </div>
+            </section>
+        </>
     )
 }
 
