77e1b64d00b18bb58e1023c98be58527e4b42b2f
Yin Lynn Htun <yinlynnhtun@gmail.com>
2022-01-30 00:51:23 +0630

    #88 feat: Display feedback alert

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -3,13 +3,41 @@ import { Link } from 'react-router-dom'
 import IntroSection from '../../components/IntroSection/IntroSection'
 import classes from './Login.module.css'
 import LoginForm from './LoginForm'
-
+import { useSelector } from 'react-redux'
+import Alert from '../../components/UI/Alert/Alert'
 const Login = () => {
+    const { error, loginData } = useSelector((state) => state.auth)
+
+    let alert
+
+    if (error) {
+        alert = (
+            <Alert
+                className={classes.alert__message}
+                alert_type="danger"
+                icon="times"
+            >
+                {error.message}
+            </Alert>
+        )
+    } else if (loginData) {
+        alert = (
+            <Alert
+                className={classes.alert__message}
+                alert_type="info"
+                icon="times"
+            >
+                Login Successfully
+            </Alert>
+        )
+    }
+
     return (
         <div className={classes.container}>
             <IntroSection />
             <section className={classes.split}>
                 <div className={classes.content}>
+                    {alert}
                     <p className={classes.title}>Hello Again!</p>
                     <p className={classes.subtitle}>Welcome Back</p>
                     <LoginForm />
