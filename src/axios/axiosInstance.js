1d4ba13fb85276a7345c043a9fcd06278c720fee
Yin Lynn Htun <yinlynnhtun@gmail.com>
2022-02-08 00:22:44 +0630

    feat: Change timeout to 5s

--- src/axios/axiosInstance.js
+++ src/axios/axiosInstance.js
@@ -1,8 +1,8 @@
-import axios from "axios";
+import axios from 'axios'
 
 const instance = axios.create({
     baseURL: 'http://168.138.215.26:9000/',
-    timeout: 1000
-});
-  
+    timeout: 5000,
+})
+
 export default instance
