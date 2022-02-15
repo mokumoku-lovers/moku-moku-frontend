e67d86161db794abcb3a26a3611c17dee4557625
Yin Lynn Htun <yinlynnhtun@gmail.com>
2022-02-15 15:50:54 +0630

    #93 feat: Use user data from store

--- src/pages/ProfileEditingPage/GeneralInfoForm/GeneralInfoForm.js
+++ src/pages/ProfileEditingPage/GeneralInfoForm/GeneralInfoForm.js
@@ -3,11 +3,15 @@ import Input from '../../../components/UI/Input/Input'
 import TextArea from '../../../components/UI/TextArea/TextArea'
 import classes from './GeneralInfoForm.module.css'
 import Button from '../../../components/UI/Button/Button'
+import { useSelector } from 'react-redux'
+
 const GeneralInfoForm = () => {
-    const [name, setName] = useState('')
-    const [username, setUsername] = useState('')
-    const [email, setEmail] = useState('')
-    const [bio, setBio] = useState('')
+    const { user } = useSelector((store) => store.user)
+
+    const [name, setName] = useState(user.display_name)
+    const [username, setUsername] = useState(user.username)
+    const [email, setEmail] = useState(user.email)
+    const [bio, setBio] = useState(user.bio)
 
     const onNameChangeHandler = (event) => {
         setName(event.target.value)
