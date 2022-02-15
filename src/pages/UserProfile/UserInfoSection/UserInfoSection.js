bcbdfea56a4fcc21d7c43d0aff5cc93c157004d8
Yin Lynn Htun <yinlynnhtun@gmail.com>
2022-02-15 14:39:31 +0630

    feat: Use state value from redux store

--- src/pages/UserProfile/UserInfoSection/UserInfoSection.js
+++ src/pages/UserProfile/UserInfoSection/UserInfoSection.js
@@ -5,9 +5,13 @@ import Point from '../../../components/UI/Point/Point'
 import { Avatar } from '../../../components/UI/Avatar/Avatar'
 import icon from '../../../icon.svg'
 import { useHistory } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 const UserInfoSection = () => {
     const history = useHistory()
+    const { username, biography, points } = useSelector(
+        (store) => store.user.user
+    )
 
     const onClickCreateButton = () => {
         history.push('/create-deck')
@@ -17,9 +21,11 @@ const UserInfoSection = () => {
         <div className={classes.container}>
             <Avatar className={classes.avatar} src={icon} />
             <div className={classes.user_info__container}>
-                <h1 className={classes.user__info__name}>User#8321</h1>
-                <p className={classes.user__info__bio}>Seize the day</p>
-                <Point point={7000} />
+                <h1 className={classes.user__info__name}>{username}</h1>
+                <p className={classes.user__info__bio}>
+                    {biography ? biography : 'There is no bio yet!'}
+                </p>
+                <Point point={points} />
                 <div className={classes.user_info__actions}>
                     <Button onClick={onClickCreateButton}>Create</Button>
                     <Button>Edit</Button>
