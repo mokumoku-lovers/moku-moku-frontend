9fbee44d8d381b4001808bf4195c73e0bdf51204
Yin Lynn Htun <yinlynnhtun@gmail.com>
2022-02-20 00:21:05 +0630

    #93 feat: Add redux persist to lose data on reload

--- src/index.js
+++ src/index.js
@@ -2,14 +2,54 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import { store } from './app/store'
+import { rootReducer } from './app/store'
 import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { PersistGate } from 'redux-persist/integration/react'
+
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [
+                    FLUSH,
+                    REHYDRATE,
+                    PAUSE,
+                    PERSIST,
+                    PURGE,
+                    REGISTER,
+                ],
+            },
+        }),
+})
+
+let persistor = persistStore(store)
 
 ReactDOM.render(
-    <React.StrictMode>
-        <Provider store={store}>
+    <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
             <App />
-        </Provider>
-    </React.StrictMode>,
+        </PersistGate>
+    </Provider>,
     document.getElementById('root')
 )
