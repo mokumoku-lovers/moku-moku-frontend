f2804799f40392a3a552c4fa2caf4761dfd025db
Yin Lynn Htun <yinlynnhtun@gmail.com>
2022-05-21 23:20:27 +0630

    feat: Change home section

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import NavBar from '../../components/NavBar/NavBar'
 import classes from './Homepage.module.css'
-import girl from './girl.png'
 import wave2 from './wave2.svg'
 import card from './cardImages/card.png'
 import cardWithAnswer from './cardImages/cardWithAnswer.png'
 import cardWithImage from './cardImages/cardWithImage.png'
 import leaderboard from './cardImages/leaderboard.png'
-
+import CardImg from './Cards.png'
 const Homepage = () => {
     return (
         <>
@@ -15,13 +14,19 @@ const Homepage = () => {
             <main className={classes.main}>
                 <section className={classes.heroSection}>
                     <div className={classes.content}>
-                        <h1>CREATE IT. REMEMBER IT</h1>
-                        <h3>Master any subject, one success at a time</h3>
-                        <button>Get Started</button>
-                    </div>
-
-                    <div className={classes.image}>
-                        <img src={girl} alt="hero" />
+                        <div className={classes.leftContent}>
+                            <h1>CREATE IT. </h1>
+                            <h1>REMEMBER IT</h1>
+                            <h3>Master any subject, one success at a time</h3>
+                            <button>Get Started</button>
+                        </div>
+                        <div className={classes.rightContent}>
+                            <img
+                                className={classes.cardImg}
+                                src={CardImg}
+                                alt="cards"
+                            />
+                        </div>
                     </div>
                 </section>
 
