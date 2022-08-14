2e63fe51256ad1df32c5f9b50198c0a378e3876b
Yin Lynn Htun <yinlynnhtun@better.hr>
2022-08-14 21:02:42 +0630

    feat: Redux logic for deck api

--- src/features/deckTitle/deckSlice.js
+++ src/features/deckTitle/deckSlice.js
@@ -1,8 +1,64 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import axios from '../../axios/axiosInstanceFunction'
 
 const initialState = {
+    id: '',
     title: '',
+    cards: [],
 }
+
+export const createDeck = createAsyncThunk(
+    'deck/createDeck',
+    async (formData, { rejectWithValue }) => {
+        try {
+            const response = await axios('http://168.138.215.26:9002/').post(
+                '/deck',
+                formData
+            )
+            return { id: response.data, title: formData.name }
+        } catch (err) {
+            if (err.response) {
+                console.log(err.response.data.message)
+                return rejectWithValue(err.response.data.message)
+            }
+        }
+    }
+)
+
+// export const getUserDeck = createAsyncThunk(
+//     'deck/getUserDeck',
+//     async (_, { rejectWithValue }) => {
+//         try {
+//             const response = await axios('http://168.138.215.26:9002/').get(
+//                 '/decks/168'
+//             )
+//             return response.data
+//         } catch (err) {
+//             if (err.response) {
+//                 console.log(err.response.data.message)
+//                 return rejectWithValue(err.response.data.message)
+//             }
+//         }
+//     }
+// )
+
+export const getDeckById = createAsyncThunk(
+    'deck/getDeckById',
+    async (deckId, { rejectWithValue }) => {
+        try {
+            const response = await axios('http://168.138.215.26:9002/').get(
+                `/deck/${deckId}`
+            )
+            console.log('get deck by id', response.data)
+            return response.data
+        } catch (err) {
+            if (err.response) {
+                console.log(err.response.data.message)
+                return rejectWithValue(err.response.data.message)
+            }
+        }
+    }
+)
 
 export const deckSlice = createSlice({
     name: 'deck',
@@ -11,6 +67,22 @@ export const deckSlice = createSlice({
         onSaveTitle: (state, action) => {
             state.title = action.payload
         },
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(createDeck.fulfilled, (state, action) => {
+                state.title = action.payload.title
+                state.id = action.payload.id
+            })
+            .addCase(getDeckById.fulfilled, (state, action) => {
+                state.id = action.payload.id
+                state.cards = action.payload.cards
+                state.title = action.payload.name
+            })
+        // .addCase(getUserDeck.fulfilled, (state, action) => {
+        //     state.title = action.payload.title
+        //     state.id = action.payload.id
+        // })
     },
 })
 
