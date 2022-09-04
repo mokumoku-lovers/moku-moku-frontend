b4008b2298f22bb719f500b2054b13972bea5844
Yin Lynn Htun <yinlynnhtun@better.hr>
2022-09-04 17:34:44 +0630

    fetch the deck by id when the page load

--- src/pages/DeckDetail/DeckDetail.js
+++ src/pages/DeckDetail/DeckDetail.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import { useParams } from 'react-router-dom/cjs/react-router-dom.min'
 import NavBar from '../../components/NavBar/NavBar'
+import { getDeckById } from '../../features/deckTitle/deckSlice'
 import CardItems from './CardItems'
 import classes from './DeckDetail.module.css'
 import DeckDetailPageHeader from './DeckDetailPageHeader'
 
 const DeckDetailPage = (props) => {
     const { deckId } = useParams()
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        const fetchDeck = async () => {
+            await dispatch(getDeckById(deckId))
+        }
+
+        fetchDeck()
+    }, [deckId, dispatch])
 
     return (
         <>
