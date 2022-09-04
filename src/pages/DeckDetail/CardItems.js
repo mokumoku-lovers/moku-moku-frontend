0e34be57187ad2b31cf27502dc7e2cd1d3fbf66d
Yin Lynn Htun <yinlynnhtun@better.hr>
2022-09-04 17:35:11 +0630

    Get the list of cards from the store

--- src/pages/DeckDetail/CardItems.js
+++ src/pages/DeckDetail/CardItems.js
@@ -2,34 +2,11 @@ import React, { useEffect } from 'react'
 import Button from '../../components/UI/Button/Button'
 import classes from './CardItems.module.css'
 import { useHistory } from 'react-router-dom'
-import { useState } from 'react'
-import axios from '../../axios/axiosInstanceFunction'
-import { useDispatch } from 'react-redux'
-import { getDeckById } from '../../features/deckTitle/deckSlice'
+import { useSelector } from 'react-redux'
 
 const CardItems = ({ deckId }) => {
     const history = useHistory()
-
-    const [cardList, setCardList] = useState([])
-    const dispatch = useDispatch()
-
-    useEffect(() => {
-        const fetchCard = async () => {
-            const response = await dispatch(getDeckById(deckId))
-            const cardIds = response.payload.cards
-
-            const responses = await Promise.all(
-                cardIds.map((cardId) =>
-                    axios('http://168.138.215.26:9002/').get(`card/${cardId}`)
-                )
-            )
-
-            const cards = responses.map((res) => res.data)
-            setCardList(cards)
-        }
-
-        fetchCard()
-    }, [deckId, dispatch])
+    const { cards: cardList } = useSelector((store) => store.deck)
 
     const onClickEditHandler = (id) => {
         history.push(`/card/${id}`)
