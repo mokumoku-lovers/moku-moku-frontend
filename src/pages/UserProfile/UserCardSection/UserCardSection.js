9dce7f0c0be7b53b9062c6113fb23f93dcb7d704
JiahuiChen99 <Jiahui1@hotmail.es>
2022-10-09 08:17:38 +0200

    #108 refactor: card section styles and positiing

--- src/pages/UserProfile/UserCardSection/UserCardSection.js
+++ src/pages/UserProfile/UserCardSection/UserCardSection.js
@@ -5,8 +5,8 @@ import classes from './UserCardSection.module.css'
 const UserCardSection = () => {
     return (
         <div className={classes.container}>
+            <h1 className={classes.title}>All Card</h1>
             <div className={classes.card_section}>
-                <h1 className={classes.title}>All Card</h1>
                 <UserProfileCard title={"Japanese N5"} text="100 vocabs" />
                 <UserProfileCard title={"TOFEL"} text="100 vocabs" />
             </div>
