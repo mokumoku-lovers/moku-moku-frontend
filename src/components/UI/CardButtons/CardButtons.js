2051d8225967945cc1ab50873317850f17617e62
Yin Lynn Htun <yinlynnhtun@gmail.com>
2022-10-16 14:54:55 +0630

    feat: remove time in show answer buttons

--- src/components/UI/CardButtons/CardButtons.js
+++ src/components/UI/CardButtons/CardButtons.js
@@ -7,12 +7,12 @@ import { setCurrentCardIdx } from '../../../features/study/studySlice'
 const buttonConfig = {
     front: [{ name: 'Previous' }, { name: 'Show Answer' }, { name: 'Edit' }],
     back: [
-        { name: 'Previous', time: '' },
-        { name: 'Again', time: '<1m' },
-        { name: 'Hard', time: '1d' },
-        { name: 'Good', time: '4d' },
-        { name: 'Easy', time: '10d' },
-        { name: 'Edit', time: '' },
+        { name: 'Previous' },
+        { name: 'Again' },
+        { name: 'Hard' },
+        { name: 'Good' },
+        { name: 'Easy' },
+        { name: 'Edit' },
     ],
 }
 
@@ -48,8 +48,6 @@ const CardButtons = ({ cardItem, cardState, parentCallback, isFinal }) => {
     const MakeButtons = (side) => {
         return buttonConfig[side].map((buttonItem) => (
             <div key={buttonItem.name} className={buttonItem.name}>
-                <h1 className={classes.buttonHeading}>{buttonItem.time}</h1>
-
                 <button
                     disabled={
                         buttonItem.name === 'Previous' && currentCardIdx === 0
