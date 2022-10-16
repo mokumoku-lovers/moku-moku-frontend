0d1ecc94b565bbade54b302ae66bff9c858fe306
Yin Lynn Htun <yinlynnhtun@gmail.com>
2022-10-16 17:02:12 +0630

    remove console logs

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -87,7 +87,6 @@ export const updateUserPoint = createAsyncThunk(
                 `/users/${userId}`,
                 { points }
             )
-            console.log(response.data)
             return response.data
         } catch (err) {
             console.dir(err)
