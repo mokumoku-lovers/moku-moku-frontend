d4c890478ee88dcae24b407ce4ee9940fd742516
yinlynnhtun <yinlynnhtun@rezeve.com>
2022-11-06 01:46:43 +0630

    fix: Fix image overlapping in ranking page

--- src/pages/RankingPage/RankingPage.js
+++ src/pages/RankingPage/RankingPage.js
@@ -6,6 +6,12 @@ import Badge from '../../components/UI/Badge/Badge'
 import Button from '../../components/UI/Button/Button'
 import ButtonSecondary from '../../components/UI/Button/ButtonSecondary'
 import Point from '../../components/UI/Point/Point'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import axios from '../../axios/axiosInstanceFunction'
+import RankingPageLoader from '../../components/Ranking/RankingPageLoader'
+import Icon from '../../icon.svg'
+
 const mockData = [
     {
         name: 'User1',
@@ -31,28 +37,54 @@ const mockData = [
 ]
 
 const RankingPage = () => {
+    const [users, setUsers] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                const response = await axios('http://168.138.215.26:9000').get(
+                    '/users'
+                )
+                console.log(response, 'This is the users')
+            } catch (err) {
+                console.log(err)
+            } finally {
+                setTimeout(() => {
+                    setIsLoading(false)
+                }, 2000)
+            }
+        }
+
+        fetchUsers()
+    }, [])
+
     return (
         <div className={classes.container}>
             <h1>Leaderboard</h1>
-            <section>
-                <Avatar className={classes.avatar} />
-                <Badge className={classes.badge} />
-                <Point className={classes.point} point={7000} />
+            {isLoading ? (
+                <RankingPageLoader />
+            ) : (
+                <section>
+                    <Avatar className={classes.avatar} src={Icon} />
+                    <Badge className={classes.badge} />
+                    <Point className={classes.point} point={7000} />
 
-                <div className={classes.buttons}>
-                    <ButtonSecondary>Friends</ButtonSecondary>
-                    <Button>Global</Button>
-                </div>
-                {mockData.map((item) => (
-                    <RankingListItem
-                        key={item.name}
-                        name={item.name}
-                        img_src={item.img_src}
-                        point={item.point}
-                        rank={item.rank}
-                    />
-                ))}
-            </section>
+                    <div className={classes.buttons}>
+                        <ButtonSecondary>Friends</ButtonSecondary>
+                        <Button>Global</Button>
+                    </div>
+                    {mockData.map((item) => (
+                        <RankingListItem
+                            key={item.name}
+                            name={item.name}
+                            img_src={item.img_src}
+                            point={item.point}
+                            rank={item.rank}
+                        />
+                    ))}
+                </section>
+            )}
         </div>
     )
 }
