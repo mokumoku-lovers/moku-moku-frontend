a846140133575e50cde9eeee158a46101052c279
yinlynnhtun <yinlynnhtun@rezeve.com>
2022-11-06 19:56:47 +0630

    Added for NavBar in ranking page

--- src/pages/RankingPage/RankingPage.js
+++ src/pages/RankingPage/RankingPage.js
@@ -11,6 +11,7 @@ import { useState } from 'react'
 import axios from '../../axios/axiosInstanceFunction'
 import RankingPageLoader from '../../components/Ranking/RankingPageLoader'
 import Icon from '../../icon.svg'
+import NavBar from '../../components/NavBar/NavBar'
 
 const RankingPage = () => {
     const [users, setUsers] = useState([])
@@ -36,34 +37,42 @@ const RankingPage = () => {
     }, [])
 
     return (
-        <div className={classes.container}>
-            <h1>Leaderboard</h1>
-            {isLoading ? (
-                <RankingPageLoader />
-            ) : (
-                <section className={classes.leaderboardContent}>
-                    <Avatar className={classes.avatar} src={Icon} />
-                    <Badge className={classes.badge} />
-                    <h1 className={classes.username}>{users[0].username}</h1>
-                    <Point className={classes.point} point={users[0].points} />
-
-                    <div className={classes.buttons}>
-                        <ButtonSecondary>Friends</ButtonSecondary>
-                        <Button>Global</Button>
-                    </div>
-                    {users.slice(2).map((item, idx) => (
-                        <RankingListItem
-                            key={item.name}
-                            name={item.username}
-                            img_src={
-                                'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxhbGx8fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit'
-                            }
-                            point={item.points}
-                            rank={idx + 2}
+        <div>
+            <NavBar />
+            <div className={classes.container}>
+                <h1>Leaderboard</h1>
+                {isLoading ? (
+                    <RankingPageLoader />
+                ) : (
+                    <section className={classes.leaderboardContent}>
+                        <Avatar className={classes.avatar} src={Icon} />
+                        <Badge className={classes.badge} />
+                        <h1 className={classes.username}>
+                            {users[0].username}
+                        </h1>
+                        <Point
+                            className={classes.point}
+                            point={users[0].points}
                         />
-                    ))}
-                </section>
-            )}
+
+                        <div className={classes.buttons}>
+                            <ButtonSecondary>Friends</ButtonSecondary>
+                            <Button>Global</Button>
+                        </div>
+                        {users.slice(2).map((item, idx) => (
+                            <RankingListItem
+                                key={item.name}
+                                name={item.username}
+                                img_src={
+                                    'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxhbGx8fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit'
+                                }
+                                point={item.points}
+                                rank={idx + 2}
+                            />
+                        ))}
+                    </section>
+                )}
+            </div>
         </div>
     )
 }
