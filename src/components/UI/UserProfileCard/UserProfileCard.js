d0b55a9203814a057a992e7f417a1eed550f34fa
yinlynnhtun <yinlynnhtun@rezeve.com>
2022-11-06 22:49:25 +0630

    feat: Added Delete icon in deck when hover

--- src/components/UI/UserProfileCard/UserProfileCard.js
+++ src/components/UI/UserProfileCard/UserProfileCard.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import classes from './UserProfileCard.module.css'
+import Trash from '../../../assets/images/trash.svg'
 
 const UserProfileCard = (props) => {
     return (
         <div className={classes.container}>
+            <img className={classes.trashIcon} src={Trash} alt="Trash" />
             <p className={classes.title}>{props.title}</p>
             <p className={classes.text}>{props.text}</p>
         </div>
