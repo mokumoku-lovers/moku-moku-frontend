46dd66817bc0a85cf2e533b17dfef0b4ad88ddbd
yinlynnhtun <yinlynnhtun@rezeve.com>
2023-01-22 17:39:52 +0630

    feat: Add noti slice

--- src/app/store.js
+++ src/app/store.js
@@ -2,6 +2,7 @@ import authReducer from '../features/auth/authSlice'
 import userReducer from '../features/user/userSlice'
 import deckReducer from '../features/deckTitle/deckSlice'
 import studyReducer from '../features/study/studySlice'
+import notiReducer from '../features/notification/notificationSlice'
 import { combineReducers } from '@reduxjs/toolkit'
 import storage from 'redux-persist/lib/storage'
 
@@ -10,11 +11,11 @@ const appReducers = combineReducers({
     user: userReducer,
     deck: deckReducer,
     study: studyReducer,
+    noti: notiReducer,
 })
 
-
-export const rootReducer = (state, action )=>{
-    if(action.type === 'logout'){
+export const rootReducer = (state, action) => {
+    if (action.type === 'logout') {
         storage.removeItem('persist:root')
         localStorage.clear()
         return appReducers(undefined, action)
